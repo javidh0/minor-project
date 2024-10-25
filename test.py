ad981b880af60096c0109f54a7eaecd286f39ebb
javidh0 <120474876+javidh0@users.noreply.github.com>
2024-10-25 15:30:39 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from FeatureProcessing.main import *
 from FeatureProcessing import roi
 
-video = VideoFeature("C:\\Users\\JAVIDH S\\Downloads\\vid.avi", getRoiCallback=roi.getROI, maxObjects=2)
+video = VideoFeature("C:\\Users\\JAVIDH S\\Downloads\\vid.avi", "C:\\Users\\JAVIDH S\\Downloads\\gtdump (1).xmp", getRoiCallback=roi.getROI, maxObjects=2, isXmp=True)
 
 video.readVideo()
