65421f8f25a2ea6a481059cb5af34893a94bc76a
javidh0 <120474876+javidh0@users.noreply.github.com>
2024-10-29 10:55:39 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from FeatureProcessing.main import *
 from FeatureProcessing import roi
 
-video = VideoFeature("D:\\UBFC Dataset\\subject1\\vid.avi", "D:\\UBFC Dataset\\subject1\\ground_truth.txt", getRoiCallback=roi.getROI, maxObjects=2)
+video = VideoFeature("D:\\UBFC Dataset\\subject1\\vid.avi", "D:\\UBFC Dataset\\subject1\\ground_truth.txt", getRoiCallback=roi.getROI, maxObjects=5)
 
 video.readVideo()
