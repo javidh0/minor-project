d2133bea24ab539cafa8a2bdc43d0381085de788
javidh0 <120474876+javidh0@users.noreply.github.com>
2024-10-29 12:20:58 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 from FeatureProcessing.main import *
 from FeatureProcessing import roi
 
-video = VideoFeature("D:\\UBFC Dataset\\subject1\\vid.avi", "D:\\UBFC Dataset\\subject1\\ground_truth.txt", getRoiCallback=roi.getROI, maxObjects=5)
+video = VideoFeature("D:\\UBFC Dataset\\subject1\\vid.avi", "D:\\UBFC Dataset\\subject1\\ground_truth.txt", getRoiCallback=roi.getROI, maxObjects=1)
 
 video.readVideo()
+
+chrom = ChormFeatures(video)
+chrom.buildCHROM()
+
+print(chrom.getFeatureImage()[0][0].shape)
