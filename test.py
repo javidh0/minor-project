5a676675259fa0b808de4aee82610208dd76e163
javidh0 <120474876+javidh0@users.noreply.github.com>
2024-10-31 09:40:54 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,33 @@
 from FeatureProcessing.main import *
 from FeatureProcessing import roi
+import matplotlib.pyplot as plt
+import numpy as np
 
-video = VideoFeature("D:\\UBFC Dataset\\subject1\\vid.avi", "D:\\UBFC Dataset\\subject1\\ground_truth.txt", getRoiCallback=roi.getROI, maxObjects=1)
+video = VideoFeature("D:\\UBFC Dataset\\subject1\\vid.avi", "D:\\UBFC Dataset\\subject1\\ground_truth.txt", getRoiCallback=roi.getROI, maxObjects=10)
 
 video.readVideo()
 
 chrom = ChormFeatures(video)
 chrom.buildCHROM()
 
-print(chrom.getFeatureImage()[0][0].shape)
+
+
+def DFT(signal_, plot = True):
+    N = len(signal_)
+
+    yf = np.fft.rfft(signal_)
+    xf = np.fft.rfftfreq(N, 1/30)
+
+    if(plot):
+        plt.plot(xf, np.abs(yf))
+        plt.xlabel('Frequency (Hz)')
+        plt.show()
+
+for i in range(10):
+    print(chrom.getFeatureImage()[i][1].mode/60)
+
+    plt.plot(chrom.getFeatureImage()[i][2])
+    plt.show()
+
+
+    DFT(chrom.getFeatureImage()[0][2])
